2f846ab6492ad46c8eab18030fbcccb9964e8bab
Akash <akashbaghel121@gmail.com>
2020-10-01 12:26:58 +0530

    First Assignment

--- index.js
+++ index.js
@@ -3,18 +3,27 @@ const http = require('http');
 const morgan = require('morgan'); 
 const bodyParser = require('body-parser');
 
+//Importing the local Modules
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
-
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+//app.use('/dishes/:dishId',dishRouter);
+
+app.use('/promotions', promoRouter);
+//app.use('/promotions/:promoId',promoRouter);
+
+app.use('/leaders', leaderRouter);
+//app.use('/leaders/:leaderId',leaderRouter);
 
 app.use(express.static(__dirname + '/public'));
 
